d04aaebc540462104b225e08e26f78de4916a2c7
j701u05 <j701u05@J701AT05.J504.kansai-center.ac.jp>
2020-12-21 09:19:37 +0900

    12/21morning

---
+++ src/december10/ChildStatic.java
@@ -0,0 +1,6 @@
+package december10;
+
+public class ChildStatic extends ParentStatic{
+	public static String sample = "子クラスです";
+
+}
---
+++ src/december10/ParentStatic.java
@@ -0,0 +1,11 @@
+package december10;
+
+public class ParentStatic {
+	public static String sample = "親クラスです";
+	public static String hoge = "ホゲ";
+
+	public static void name() {
+		System.out.println("staticメソッド");
+	}
+
+}
