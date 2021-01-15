9c33d121bb393774aa7faa3874bdbf1baa680d06
j701u05 <j701u05@J701AT05.J504.kansai-center.ac.jp>
2021-01-15 15:47:36 +0900

    サンプルプッシュ

--- src/december24/Q8.java
+++ src/december24/Q8.java
@@ -4,14 +4,15 @@ public class Q8 {
 	public static void main(String[] args) {
 //		int[][] array = new int[3][3000000000L];
 
-//		int[][] array = new int[3][];
+		int[][] array = new int[3][];
+
 
 //		int[][] array = {1,2,3};
 
 //		int[][] array;
 //		array = {{1,2},{3,4}};
 
-		int []array []= {{1,2},{3,4}};
+//		int []array []= {{1,2},{3,4}};
 
 	}
 }
--- src/december24/Sample.java
+++ src/december24/Sample.java
@@ -1,5 +1,13 @@
 package december24;
 
-public interface Sample{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Sample {
+	String[] strings = new String[]{"A","B","C"};
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>(Arrays.asList(new String[] {"A","B","C"}));
+	}
 }
